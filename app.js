b7d23999dbb513d160dd8af220181a32ac5eb3c0
khooinguyeen <khoinguyenmai17102005@gmail.com>
2022-08-07 18:41:43 +0700

    added light mode :eyes:

--- app.js
+++ app.js
@@ -33,6 +33,13 @@ function PageTransitions(){
         }
     })
 
+    //Toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',() =>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
+
 }
 
 PageTransitions()
